b7f59f1a6028a363eb3416960386184232f02934
Selim <webisdeveloper@gmail.com>
2023-03-21 15:23:26 +0300

    Ejs file add

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 
+
+// Uygulamamızda ejs modülünü kullanacağımızı belirtiyoruz
+app.set('view engine', 'ejs');
+
+// Public klasörü oluşturup içerisinde statik olan dosyaları aktardık
+// İlgili middleware fonksiyonunu oluşturarak public klasörünü uygulamamıza kaydedicez.
+app.use(express.static('public'));
+
 app.get('/',(req, res)=>{
-   const blog = {
-    id: 1,
-    title: 'Blog Title',
-    description: "Blog description"
-   }
-   res.send(blog);
+    res.render('index');
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about');
+})
+
+app.get('/add',(req, res)=>{
+    res.render('add_post');
+})
+
+app.get('/post',(req, res)=>{
+    res.render('post');
 })
 
 const port = 3000;
